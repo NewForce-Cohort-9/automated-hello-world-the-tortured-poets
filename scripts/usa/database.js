47a83d15715f1d424ad78722e518f8d742415922
Daniel Miller <dmiller3830@gmail.com>
2024-04-22 11:32:39 -0400

    dan's usa database

---
+++ scripts/usa/database.js
@@ -0,0 +1,179 @@
+const database = {
+
+    cities: [
+
+       {
+
+         
+
+        id: 1, 
+        name: "Morgantown", 
+        Population: 30277
+
+
+
+
+       }, 
+
+
+        {
+            id: 2, 
+            name: "Baltimore", 
+            population: 569531
+
+        }, 
+
+        {
+            
+            id: 3, 
+            name:  "Pittsburgh", 
+            population: 302898
+
+        }
+
+
+    ], 
+        
+
+
+      landmarks: [ 
+        
+        {
+            
+            id: 1, 
+            name: "Grand Canyon", 
+            location: "Arizona",  
+            facts: `The Grand Canyon (Hopi: Öngtupqa,[2] Yavapai: Wi:kaʼi:la, Navajo: Bidááʼ Haʼaztʼiʼ Tsékooh,[3][4] Southern Paiute language: Paxa’uipi,[5] Spanish: Gran Cañón or Gran Cañón del Colorado) 
+            is a steep-sided canyon carved by the Colorado River 
+            in Arizona, United States. The Grand 
+            Canyon is 277 miles (446 km) long, up to 18 miles 
+            (29 km) wide and attains a depth of over a mile 
+            (6,093 feet or 1,857 meters).[6]: 902 `
+            image:
+              `src="https://en.wikipedia.org/wiki/Grand_Canyon#/media/File:Canyon_River_Tree_(165872763).jpeg"alt="Image of Grand Canyon"`
+
+
+
+        }
+
+      {
+        
+        id: 2, 
+        name: "Statue Of Liberty", 
+        location: "New York, NY", 
+        facts: `The Statue of Liberty 
+        (Liberty Enlightening the 
+          World; French: La Liberté éclairant le monde) 
+          is a colossal neoclassical sculpture on Liberty 
+          Island in New York Harbor in New York City, United States. 
+          The copper statue, a gift from the people of France, 
+          was designed by French sculptor Frédéric Auguste Bartholdi 
+          and its metal framework was built by Gustave Eiffel. 
+          The statue was dedicated on October 28, 1886.`
+          image:
+          `src="https://en.wikipedia.org/wiki/Statue_of_Liberty#/media/File:Lady_Liberty_under_a_blue_sky_(cropped).jpg"alt="Image of Statue Of Liberty"`
+
+      }
+
+      "
+
+    
+        
+        id: 3, 
+        name: "Spruce Knob", 
+        location: "Pendelton, West Virginia", 
+        facts: `Spruce Mountain, located in eastern 
+        West Virginia, is the highest ridge of the \
+        Allegheny Mountains. The whale-backed ridge
+         extends for only 16 miles (26 km) from 
+         northeast to southwest, but several of its 
+         peaks exceed 4,500 feet (1,400 m) in 
+         elevation. The summit, 
+         Spruce Knob (4,863 ft; 1,482 m),[1] is the 
+         highest Allegheny Mountain point both 
+         in the state and the entire range, 
+         which spans four states.`
+        image:
+        `src="https://en.wikipedia.org/wiki/Spruce_Mountain_(West_Virginia)#/media/File:JudyRocks.JPG"alt="Image of Spruce Knob"`
+
+
+    ]
+
+   
+
+    celebrities: [
+
+       {  
+
+        id: 1, 
+        name: "Don Knots",  
+        image: 
+        `src="https://hips.hearstapps.com/hmg-prod/images/don-knotts-gettyimages-57656381.jpg?crop=1xw:0.5xh;center,top&resize=1200:*"alt="Image of Don Knots"`,
+        birthplace: "Morgantown, WV", 
+        bio: `In 1960, actor Don Knotts joined the sitcom 
+        The Andy Griffith Show. For his role on the show, 
+        he won three Emmy Awards for outstanding performance 
+        in a supporting role in a series. Though he left 
+        The Andy Griffith Show in 1965 to pursue a film 
+        career, his periodic returns in 1966 and '67 earned him 
+        two more Emmys. Later starring in several film comedies, 
+        Knotts also was part of the cast of 
+        Three's Company until its 1984 demise. Knotts died of 
+        lung cancer on February 24, 2006, at age 81, in Los Angeles, 
+        California.`  
+
+       },
+
+         {
+            
+            id: 2, 
+            name: "Edgar Alan Poe", 
+            image: 
+            `src="https://mediaproxy.salon.com/width/1200/https://media2.salon.com/2021/07/edgar-allan-poe-0723211.jpg"alt="Image of Edgar Alan Poe"`
+            birthplace: Boston, MA
+            bio: `Edgar Allan Poe 
+            (né Edgar Poe; January 19, 1809 – October 7, 1849) 
+            was an American writer, poet, author, editor, 
+            and literary critic who is best known for his poetry 
+            and short stories, particularly his tales of mystery 
+            and the macabre. He is widely regarded as a central 
+            figure of Romanticism and Gothic fiction in the 
+            United States, and of American literature.[1] 
+            Poe was one of the country's earliest practitioners 
+            of the short story, and is considered the inventor of 
+            the detective fiction genre, as well as a significant 
+            contributor to the emerging genre of science fiction.[2] 
+            He is the first well-known American writer to earn a living 
+            through writing alone, resulting in a financially difficult life and career.[3]`
+
+         }, 
+
+        { 
+            
+            id: 3, 
+            name: "Steve Harvey", 
+            image:
+            `src="https://akns-images.eonline.com/eol_images/Entire_Site/2020013/rs_1024x759-200113121440-1024-Steve-Harvey-Watch.jpg?fit=around%7C1024:759&output-quality=90&crop=1024:759;center,top"alt="Image of Steve Harvey"`
+            birthplace: "Welch, WV", 
+            bio: `Broderick Stephen Harvey 
+            Sr.[2][3] (born January 17, 1957) is an 
+            American television host, producer, actor, 
+            and comedian. He hosts The Steve Harvey Morning 
+            Show, Family Feud, Celebrity Family Feud, 
+            Family Feud Africa, the arbitration-based court 
+            comedy Judge Steve Harvey, and he formerly hosted 
+            the Miss Universe competition. His accomplishments 
+            include seven Daytime Emmy Awards, two Marconi Awards, 
+            and fourteen NAACP Image Awards.` 
+
+
+         }
+
+]
+
+
+      
+}
+
+       
+
